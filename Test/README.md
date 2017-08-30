01b416717c0c41be15ddc0906a127413727aab5b
Jesper Holm <holmje@chalmers.se>
2017-08-30 18:00:14 +0200

    Det var bara skit

--- Test/README.md
+++
@@ -1,2 +0,0 @@
-# Test
-My Git test repository
--- Test/text.java
+++
@@ -1,7 +0,0 @@
-/* Hey this is just a comment */
-
-/* I just edited this*/
-
-/*Denna lösningen kanske funkar bättre/*
-
-/*Färdiga lösningen, pushar denna till masterbranchen. */
